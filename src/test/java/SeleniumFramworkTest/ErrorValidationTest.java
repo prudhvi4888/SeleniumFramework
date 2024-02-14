a9b7b3c05a80545f2d6d0b072de776290a7d2228
Prudhvi_reach <reachprudhvig@gmail.com>
2024-02-14 11:37:01 -0800

    branchTest

--- src/test/java/SeleniumFramworkTest/ErrorValidationTest.java
+++ src/test/java/SeleniumFramworkTest/ErrorValidationTest.java
@@ -39,7 +39,7 @@ public class ErrorValidationTest extends BaseTest {
 	@Test
 	public void productErrorValidation() throws InterruptedException {
 		
-		String name="";
+		String name="test";
 		String itemName = "ZARA COAT 3";
 		String usename = "prudhvi.raj@gmail.com";
 		String password = "Nani4321!@";
